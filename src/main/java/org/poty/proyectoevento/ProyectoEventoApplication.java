bc252ec92e242421ea8a45c5b5b85f9b09e51ada
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2025-02-02 18:01:16 -0500

    Estado inicial de creación del proyecto

---
+++ src/main/java/org/poty/proyectoevento/ProyectoEventoApplication.java
@@ -0,0 +1,13 @@
+package org.poty.proyectoevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEventoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoEventoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/poty/proyectoevento/ProyectoEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.poty.proyectoevento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEventoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
